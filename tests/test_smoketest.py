af5e33d1f8d1e214dcd6d3551974e97218088306
Emmanuel <okoyee61@gmail.com>
2026-02-13 12:25:49 +0100

    edited test

--- tests/test_smoketest.py
+++ tests/test_smoketest.py
@@ -19,37 +19,37 @@ class TestSmoketest():
     self.driver.quit()
   
   def test_adminpage(self):
-    self.driver.get("http://127.0.0.1:5500/teton/1.6/join-step2.html?fname=Emmanuel&lname=Okoye&bizname=mandb+annnnn&biztitle=direcorate&submit=Next+Step")
+    self.driver.get("http://127.0.0.1:5500/teton/1.6/admin.html")
     self.driver.set_window_size(1295, 687)
-    self.driver.find_element(By.LINK_TEXT, "Admin").click()
-    elements = self.driver.find_elements(By.ID, "username")
+    elements = self.driver.find_elements(By.CSS_SELECTOR, ".myinput:nth-child(2)")
     assert len(elements) > 0
-    self.driver.find_element(By.ID, "username").send_keys("ty")
-    self.driver.find_element(By.ID, "password").click()
-    self.driver.find_element(By.ID, "password").send_keys("4")
-    self.driver.find_element(By.CSS_SELECTOR, ".mysubmit:nth-child(4)").click()
-    assert self.driver.find_element(By.CSS_SELECTOR, ".errorMessage").text == "Invalid username and password."
+    self.driver.find_element(By.ID, "username").click()
+    elements = self.driver.find_elements(By.CSS_SELECTOR, ".errorMessage")
+    assert len(elements) > 0
   
   def test_directorypage(self):
-    self.driver.get("http://127.0.0.1:5500/teton/1.6/index.html")
+    self.driver.get("http://127.0.0.1:5500/teton/1.6/directory.html")
     self.driver.set_window_size(1295, 687)
-    self.driver.find_element(By.LINK_TEXT, "Directory").click()
     self.driver.find_element(By.ID, "directory-grid").click()
     assert self.driver.find_element(By.CSS_SELECTOR, ".gold-member:nth-child(9) > p:nth-child(2)").text == "Teton Turf and Tree"
     self.driver.find_element(By.ID, "directory-list").click()
     assert self.driver.find_element(By.CSS_SELECTOR, ".gold-member:nth-child(9) > p:nth-child(2)").text == "Teton Turf and Tree"
   
   def test_homepage(self):
-    elements = self.driver.find_elements(By.CSS_SELECTOR, ".header-logo img")
-    assert len(elements) > 0
-    self.driver.find_element(By.ID, "nopad").click()
+    self.driver.get("http://127.0.0.1:5500/teton/1.6/index.html")
+    self.driver.set_window_size(652, 672)
     assert self.driver.find_element(By.CSS_SELECTOR, ".header-title > h1").text == "Teton Idaho"
     assert self.driver.find_element(By.CSS_SELECTOR, ".header-title > h2").text == "Chamber of Commerce"
+    assert self.driver.title == "Teton Idaho CoC"
+    elements = self.driver.find_elements(By.CSS_SELECTOR, ".header-logo img")
+    assert len(elements) > 0
     elements = self.driver.find_elements(By.CSS_SELECTOR, ".spotlight1")
     assert len(elements) > 0
     elements = self.driver.find_elements(By.CSS_SELECTOR, ".spotlight2")
     assert len(elements) > 0
-    self.driver.set_window_size(652, 678)
+    elements = self.driver.find_elements(By.LINK_TEXT, "Join Us!")
+    assert len(elements) > 0
+    self.driver.find_element(By.LINK_TEXT, "Join Us!").click()
   
   def test_joinpage(self):
     self.driver.get("http://127.0.0.1:5500/teton/1.6/index.html")
@@ -57,14 +57,7 @@ class TestSmoketest():
     self.driver.find_element(By.LINK_TEXT, "Join").click()
     elements = self.driver.find_elements(By.NAME, "fname")
     assert len(elements) > 0
-    self.driver.find_element(By.NAME, "fname").send_keys("emmanuel")
-    self.driver.find_element(By.NAME, "lname").click()
-    self.driver.find_element(By.NAME, "lname").send_keys("okoye")
-    self.driver.find_element(By.NAME, "bizname").click()
-    self.driver.find_element(By.NAME, "bizname").send_keys("mandb")
-    self.driver.find_element(By.NAME, "biztitle").click()
-    self.driver.find_element(By.NAME, "biztitle").send_keys("director")
     self.driver.find_element(By.NAME, "submit").click()
-    elements = self.driver.find_elements(By.NAME, "email")
+    elements = self.driver.find_elements(By.CSS_SELECTOR, ".myinput:nth-child(2)")
     assert len(elements) > 0
   
