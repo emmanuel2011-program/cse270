f66e243080064d8dd0fdb6f3e134723d99560c76
Emmanuel <okoyee61@gmail.com>
2026-02-13 16:46:01 +0100

    smoke test

--- .history/tests/test_smoketest_20260213164206.py
+++ .history/tests/test_smoketest_20260213164206.py
@@ -9,9 +9,6 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.chrome.options import Options
-
-
 
 class TestSmoketest():
  def setup_method(self, method):
